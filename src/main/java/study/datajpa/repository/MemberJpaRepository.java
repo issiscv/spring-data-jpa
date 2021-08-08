1404504cf8c87dcf8ebea5cf8a3ab1ecf6445180
issiscv <issiscv46@gmail.com>
2021-08-08 21:52:34 +0900

    add repository CRUD

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 import study.datajpa.domain.Member;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -12,12 +14,34 @@ public class MemberJpaRepository {
 
     private final EntityManager em;
     
-    public Long save(Member member) {
+    //저장
+    public Member save(Member member) {
         em.persist(member);
-        return member.getId();
+        return member;
     }
 
+    //단건 조회
     public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    //삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    //전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select M from Member m", Member.class).
+                getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.domain.Member;
+import study.datajpa.domain.Team;
+
+import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,14 +19,26 @@ class MemberJpaRepositoryTest {
 
     @Autowired
     private MemberJpaRepository memberJpaRepository;
+    @Autowired
+    private TeamRepository teamRepository;
 
-//    @Test
-//    void 회원_저장() {
-//        Member member = Member.createMember("김상운");
-//
-//        Long memberId = memberJpaRepository.save(member);
-//
-//        assertThat(member).isEqualTo(memberJpaRepository.findOne(memberId));
-//        assertThat(member.getUsername()).isEqualTo(memberJpaRepository.findOne(memberId).getUsername());
-//    }
+    @Test
+    public void basicCRUD() {
+        Team team1 = Team.createTeam("teamA");
+        Team team2 = Team.createTeam("teamB");
+        teamRepository.save(team1);
+        teamRepository.save(team2);
+
+        Member member1 = Member.createMember("memberA", 24, team1);
+        Member member2 = Member.createMember("memberA", 24, team1);
+        Member save1 = memberJpaRepository.save(member1);
+        Member save2 = memberJpaRepository.save(member2);
+
+        assertThat(member1).isEqualTo(memberJpaRepository.findById(save1.getId()).get());
+        assertThat(member2).isEqualTo(memberJpaRepository.findById(save2.getId()).get());
+
+        long count = memberJpaRepository.count();
+
+        assertThat(2).isEqualTo(count);
+    }
 }
